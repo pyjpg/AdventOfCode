b6f2ad8e0575c42a2773394148d7fbd93dd52011
pyjpg <titas.r@outlook.com>
2025-01-06 14:53:11 +0000

    Day-7 start

---
+++ Day6.cs
@@ -0,0 +1,159 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Drawing;
+//using System.IO;
+//using System.Linq;
+
+//namespace AdventOfCode
+//{
+//    class Program
+//    {
+//        private static int gridWidth;
+//        private static int gridHeight;
+//        private static Point startingPoint;
+//        private static char[,] grid;
+//        private static HashSet<Point> obstaclesPoint = new();
+//        private static List<Point> availablePositions = new();
+//        public static int obstacleLimit = 0;
+//        static async Task<int> Main(string[] args)
+//        {
+//            List<string> lines = new();
+//            var fileInput = "C://Users//titas//Desktop//AdventOfCode//AdventOfCode//input.txt";
+//            using (var reader = new StreamReader(fileInput))
+//            {
+
+//                while ((await reader.ReadLineAsync()) is { } line)
+//                {
+//                    gridWidth = line.Length;
+//                    gridHeight++;
+//                    lines.Add(line);
+//                }
+//            }
+
+//            grid = new char[gridWidth, gridHeight];
+
+//            // Find starting point and obstacles
+//            for (int y = 0; y < gridHeight; y++)
+//            {
+//                for (int x = 0; x < gridWidth; x++)
+//                {
+//                    grid[x, y] = lines[y][x];
+//                    if (grid[x, y] == '^')
+//                    {
+//                        startingPoint = new Point(x, y);
+//                    }
+
+//                }
+//            }
+
+//            var obstaclesPossible = ObstaclesPossible(startingPoint);
+//            int obstaclesCount = 0;
+//            foreach (var obstacle in obstaclesPossible.Except([startingPoint]))
+//            {
+//                if (DoesGuardLoop(startingPoint, obstacle))
+//                {
+//                    obstaclesCount++;
+//                }
+//            }
+//            Console.WriteLine(obstaclesCount);
+//            return obstaclesCount;
+
+
+//        }
+//        private static bool DoesGuardLoop(Point startingPoint, Point obstacle)
+//        {
+//            HashSet<(Point point, Point direction)> visited = new();
+//            var currentDirection = new Point(0, -1);
+//            var currentPoint = startingPoint;
+
+//            while (true)
+
+//            {
+//                // add an obstacle and let the guard patrol if he doesnt get into a forever loop then we dont add position for the obstacle
+//                if (visited.Contains((currentPoint, currentDirection)))
+//                {
+//                    return true;
+//                }
+//                visited.Add((currentPoint, currentDirection));
+//                var nextPosition = new Point((currentPoint.X + currentDirection.X), (currentPoint.Y + currentDirection.Y));
+
+//                if (OutOfBounds(nextPosition))
+//                {
+//                    break;
+//                }
+//                if (grid[nextPosition.X, nextPosition.Y] == '#' || (nextPosition.X == obstacle.X && nextPosition.Y == obstacle.Y))
+//                {
+//                    currentDirection = new Point(-currentDirection.Y, currentDirection.X);
+//                    nextPosition = currentPoint;
+
+//                }
+//                if (OutOfBounds(nextPosition))
+//                {
+//                    break;
+//                }
+
+//                currentPoint = nextPosition;
+//            }
+//            return false;
+//        }
+//        public static HashSet<Point> ObstaclesPossible(Point startingPoint)
+//        {
+//            HashSet<Point> visited = new();
+
+//            var currentDirection = new Point(0, -1);
+//            var currentPoint = startingPoint;
+
+//            while (true)
+
+//            {
+//                // add an obstacle and let the guard patrol if he doesnt get into a forever loop then we dont add position for the obstacle
+
+//                visited.Add(currentPoint);
+//                var nextPosition = new Point((currentPoint.X + currentDirection.X), (currentPoint.Y + currentDirection.Y));
+
+//                if (OutOfBounds(nextPosition))
+//                {
+//                    break;
+//                }
+//                if (grid[nextPosition.X, nextPosition.Y] == '#')
+//                {
+//                    currentDirection = new Point(-currentDirection.Y, currentDirection.X);
+//                    nextPosition = currentPoint;
+
+//                }
+//                if (OutOfBounds(nextPosition))
+//                {
+//                    break;
+//                }
+
+//                currentPoint = nextPosition;
+//            }
+//            return visited;
+//        }
+
+//        public static void InsertObstacle(Point obstacle)
+//        {
+
+//            if (obstacle.X < gridWidth && obstacle.Y < gridHeight && !obstaclesPoint.Add(obstacle))
+//            {
+
+//                if (obstacleLimit < 1)
+//                {
+//                    obstaclesPoint.Add(obstacle);
+//                    obstacleLimit++;
+//                    grid[obstacle.X, obstacle.Y] = 'O';
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Obstacle limit reached");
+//                }
+
+
+//            }
+//        }
+//        private static bool OutOfBounds(Point nextPoint)
+//        {
+//            return nextPoint.X < 0 || nextPoint.Y < 0 || nextPoint.X >= gridWidth || nextPoint.Y >= gridHeight;
+//        }
+//    }
+//}
--- Program.cs
+++ Program.cs
@@ -15,116 +15,79 @@ namespace AdventOfCode
         private static HashSet<Point> obstaclesPoint = new();
         private static List<Point> availablePositions = new();
         public static int obstacleLimit = 0;
-        static async Task Main(string[] args)
+        static  void Main(string[] args)
         {
-            List<string> lines = new();
+            List<int> Sums = new();
+            int sumOfTrue = 0;
+            List<List<int>> operations = new();
             var fileInput = "C://Users//titas//Desktop//AdventOfCode//AdventOfCode//input.txt";
-            using (var reader = new StreamReader(fileInput))
+            foreach (var line in File.ReadAllLines(fileInput))
             {
-
-                while ((await reader.ReadLineAsync()) is { } line)
+                List<int> currentOperations = new();
+                var parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int x = 0; x < parts.Length; x++)
                 {
-                    gridWidth = line.Length;
-                    gridHeight++;
-                    lines.Add(line);
-                }
-            }
-
-            grid = new char[gridWidth, gridHeight];
-
-            // Find starting point and obstacles
-            for (int y = 0; y < gridHeight; y++)
-            {
-                for (int x = 0; x < gridWidth; x++)
-                {
-                    grid[x, y] = lines[y][x];
-                    if (grid[x,y] == '^')
+                    if (parts[x].Contains(":")) 
                     {
-                        startingPoint = new Point(x, y);
-                    }
-                    if (grid[x, y] == '.' && obstaclesPoint.Add(new Point(x, y))) // Only add obstacle if not already added
-                    {
+                        parts[x] = parts[x].Replace(":", "");
+                        if (int.TryParse(parts[x], out var sum))
+                        {
+                            Sums.Add(sum);
+                        }
                         
-                        InsertObstacle(new Point(x, y));
-
+                    }
+                    else if (int.TryParse(parts[x], out var number))
+                    {
+                        currentOperations.Add(number);
+                        
                     }
                     
                 }
-            }
 
-            CountSteps(startingPoint).ToString();
-            
-
-
-        }
-        public static int CountSteps(Point startingPoint)
-        {
-            HashSet<Point> visited = new();
-            
-            var currentDirection = new Point(0, -1);
-            var currentPoint = startingPoint;
-            var foreverObstacle = 0;
-            var counter = 0;
-            while (true)
-
-            {
-                // add an obstacle and let the guard patrol if he doesnt get into a forever loop then we dont add position for the obstacle
-
-                visited.Add(currentPoint);
-                var nextPosition = new Point((currentPoint.X + currentDirection.X), (currentPoint.Y + currentDirection.Y));
-                if (!visited.Add(nextPosition))
+                if (currentOperations.Count > 0)
                 {
-                    Console.WriteLine($"Forever loop: {currentPoint.X}, {currentPoint.Y}");
+                    operations.Add(currentOperations);
+                }
 
+            }
+            for (int i = 0; i < operations.Count; i++)
+            {
 
-                    counter++;
-                    if (counter > 4)
+               
+                
+                    var sum = Sums[i];
+                 
+
+                    for (int x = 0; x < operations[i].Count; x++)
                     {
-                        foreverObstacle++;
-                        return counter;
-                        
+                        for (int y = x + 1; y < operations[i].Count; y++)
+                        {
+                            var operationX = operations[i][x];
+                            var operationY = operations[i][y];
+
+
+                            var combo1 = operationX * operationY;
+                            var combo2 = operationX + operationY;
+
+
+                            if (sum == combo1 || sum == combo2)
+                            {
+                                Console.WriteLine($"The operations match the sum: {sum} using operations {operationX} and {operationY}");
+                                sumOfTrue += sum;
+                            }
+                        }
                     }
-                }
-                if (OutOfBounds(nextPosition))
-                {
-                    break;
-                }
-                if (grid[nextPosition.X, nextPosition.Y] == '#' || grid[nextPosition.X, nextPosition.Y] == 'O')
-                {
-                    currentDirection = new Point(-currentDirection.Y, currentDirection.X);
-                    nextPosition = new Point(
-                        currentPoint.X + currentDirection.X,
-                        currentPoint.Y + currentDirection.Y
-                    );
-                }
-                if (OutOfBounds(nextPosition))
-                {
-                    break;
-                }
-
-                currentPoint = nextPosition;
-            }
-            return visited.Count;
-
-
-
-        }
-
-        public static void InsertObstacle(Point obstacle)
-        {
-            
-            if (obstacle.X < gridWidth && obstacle.Y < gridHeight && obstacleLimit < 1 )
-            {
-
-                    obstaclesPoint.Add(obstacle);
-                    obstacleLimit++;
-                    grid[obstacle.X, obstacle.Y] = 'O';
+                
                 
             }
+            Console.WriteLine(sumOfTrue);
+
+            Console.ReadLine();
+
+
+
+
         }
-        private static bool OutOfBounds(Point nextPoint)
-        {
-            return nextPoint.X < 0 || nextPoint.Y < 0 || nextPoint.X >= gridWidth || nextPoint.Y >= gridHeight;
-        }
+       
     }
 }
